f3f284594bae7a626f604a05b466159e8e8a6aeb
Alone2 <alain.sinzig@gmail.com>
2019-12-31 06:26:16 +0100

    moving to sway

---
+++ install.sh
@@ -0,0 +1,19 @@
+echo WARNING: RUN SCRIPT AS USER NOT ROOT
+
+sudo pacman -S ttf-fira-code gnu-free-fonts ttf-hack noto-fonts ttf-roboto dmenu sway rxvt-unicode i3blocks firefox xorg-server-xwayland base-devel dunst
+
+mkdir ~/.config
+mkdir ~/.config/sway
+mkdir ~/.config/i3blocks
+
+sudo cp .config/sway/config ~/.config/sway/config
+sudo cp .Xdefaults ~/.Xdefaults
+sudo cp .config/i3blocks/* ~/.config/i3blocks/
+sudo cp .bashrc ~/.bashrc
+
+mkdir ~/Temp
+cd ~/Temp
+git clone https://aur.archlinux.org/yay.git
+cd yay 
+makepkg -si
+
