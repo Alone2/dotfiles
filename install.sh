251ea0a3fa0a43aa8cba9539d9d268369aec6695
Alone2 <alain.sinzig@gmail.com>
2020-01-01 03:40:54 +0100

    added some things

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 echo WARNING: RUN SCRIPT AS USER NOT ROOT
 
-sudo pacman -S ttf-fira-code gnu-free-fonts ttf-hack noto-fonts ttf-roboto rofi sway rxvt-unicode i3blocks firefox xorg-server-xwayland base-devel dunst
+sudo pacman -S ttf-fira-code gnu-free-fonts ttf-hack noto-fonts ttf-roboto rofi sway rxvt-unicode i3blocks firefox xorg-server-xwayland base-devel dunst arc-gtk-theme networkmanager network-manager-applet gtk3
 
 mkdir ~/.config
 mkdir ~/.config/sway
 mkdir ~/.config/i3blocks
+mkdir ~/.config/environment.d
 
 sudo cp .config/sway/config ~/.config/sway/config
 sudo cp .Xdefaults ~/.Xdefaults
+sudo cp .config/environment.d/envvars.conf ~/.config/environment.d/envvars.conf 
 sudo cp .config/i3blocks/* ~/.config/i3blocks/
 sudo cp .bashrc ~/.bashrc
 sudo cp .bash_profile ~/.bash_profile
