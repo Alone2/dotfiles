6ce677e8831818b3bbadff5be27ddb70590f0b9e
Alone2 <alain.sinzig@gmail.com>
2020-01-13 21:50:14 +0100

    bug fix

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ mkdir ~/.config/rofi
 
 sudo cp .config/i3/config ~/.config/i3/config
 sudo cp .Xresources ~/.Xresources
-sudo cp .config/i3blocks ~/.config/
-sudo cp .config/dunst ~/.config/
-sudo cp .config/compton ~/.config/
-sudo cp .config/rofi ~/.config/
+sudo cp -r .config/i3blocks ~/.config/
+sudo cp -r .config/dunst ~/.config/
+sudo cp -r .config/compton ~/.config/
+sudo cp -r .config/rofi ~/.config/
 sudo cp .bashrc ~/.bashrc
 sudo cp .bash_profile ~/.bash_profile
 sudo cp .xinitrc ~/.xinitrc
