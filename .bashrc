06d184cfa444fd4a7907488f827408b8e6c1e4ba
Alone2 <alain.sinzig@gmail.com>
2020-04-13 21:48:06 +0200

    xclip

--- .bashrc
+++ .bashrc
@@ -11,5 +11,6 @@ PS1="\[\e[1;32m\]\u@\h\[\e[1;34m\] \W \[\e[1;36m\]\$ \[\e[m\]"
 
 # use neovim
 alias vim='nvim'
-
+# xclip to copy
+alias xclipcp='xclip -selection clipboard'
 set -o vi
--- installPackages.sh
+++ installPackages.sh
@@ -2,7 +2,7 @@ echo WARNING: RUN SCRIPT AS USER NOT ROOT
 
 sudo pacman -Syu
 
-sudo pacman -S ttf-fira-code gnu-free-fonts ttf-hack noto-fonts ttf-roboto rofi i3-gaps rxvt-unicode firefox base-devel dunst gnome-themes-extra networkmanager network-manager-applet gtk3 scrot blueman neovim xorg-xinit xorg-server feh bluez pulseaudio alsa-utils redshift bdf-unifont ttf-dejavu python-pynvim clang python
+sudo pacman -S ttf-fira-code gnu-free-fonts ttf-hack noto-fonts ttf-roboto rofi i3-gaps rxvt-unicode firefox base-devel dunst gnome-themes-extra networkmanager network-manager-applet gtk3 scrot blueman neovim xorg-xinit xorg-server feh bluez pulseaudio alsa-utils redshift bdf-unifont ttf-dejavu python-pynvim clang python xclip
 
 amixer sset Master unmute
 amixer sset Speaker unmute
