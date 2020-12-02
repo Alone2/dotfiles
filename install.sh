b4e4036b1793916604025288b31cd58d1c2a99c5
Alone2 <alain.sinzig@gmail.com>
2020-12-02 20:33:12 +0100

    minimal optimisations

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 echo WARNING: RUN SCRIPT AS USER NOT ROOT
 
+# Install packages and copy dotfiles
 sh installPackages.sh
 sh copyDotfiles.sh
 sh installPackagesAUR.sh
+
+# be more confy in terminal
+set -o vi
--- installPackagesAUR.sh
+++ installPackagesAUR.sh
@@ -1,10 +1,16 @@
+# Use more cores when using make
+[ $(nproc) -gt 3 ] && alias make="/usr/bin/make -j $(expr $(nproc) / 3 \* 2)" && export MAKEFLAGS='-j '$(expr $(nproc) / 3 \* 2)
+
+# Install AUR packages
 for app in yay i3-volume urxvt-font-size-git neovim-plug siji-git polybar picom-rounded-corners ttf-font-logos
 do
  	cd /tmp
  	git clone https://aur.archlinux.org/$app.git
  	cd $app
  	makepkg -si --noconfirm
- done
+done
 
+# Install nvim plugins
 nvim -E +PlugInstall +qall
 echo "Type :q! when the installation is finished." | nvim +"CocInstall coc-clangd coc-python coc-css coc-html coc-java coc-tsserver coc-json"
+
